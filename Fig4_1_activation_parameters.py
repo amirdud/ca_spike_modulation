463da39334703dc3a388f09faf8eecbb0072783e
amirdud <amirdud@users.noreply.github.com>
2021-12-15 01:32:55 +0200

    improved modularity

--- Fig4_1_activation_parameters.py
+++ Fig4_1_activation_parameters.py
@@ -1,55 +1,50 @@
-
-# Channel specifications from Hay et al., 2011
-# ==========================================
-
-import numpy as np
-import matplotlib
-import math
 import matplotlib.pyplot as plt
-matplotlib.use('Qt5Agg')
+import channel_funs as cf
 from matplotlib import cm
+from cfg import *
 
-v_rest = -75
-v = np.arange(-100, 150, 1).astype(np.float32)
-ind_v_rest = np.where(v == v_rest)[0][0]
+general_params = cfg_fig_4_1["h"]
+v = general_params["v"]
+
+Im_params = cfg_fig_4_1["Im"]
+shift = Im_params["shift"]
+slope = Im_params["slope"]
+k = Im_params["k"]
 
 # =========== Ca_HVA ===========
-v[v == -27] = v[v == -27] + 0.001
-mAlpha = (0.055 * (-27 - v)) / (np.exp((-27 - v) / 3.8) - 1)
-mBeta = (0.94 * np.exp((-75 - v) / 17))
-mInf = mAlpha / (mAlpha + mBeta)
-mTau = 1 / (mAlpha + mBeta)
+mAlpha_CaHVA = []
+mBeta_CaHVA = []
+hAlpha_CaHVA = []
+hBeta_CaHVA = []
+mAlpha_Im = []
+mBeta_Im = []
 
-hAlpha = (0.000457 * np.exp((-13 - v) / 50))
-hBeta = (0.0065 / (np.exp((-v - 15) / 28) + 1))
-hInf = hAlpha / (hAlpha + hBeta)
-hTau = 1 / (hAlpha + hBeta)
+for vi in v:
+    mAlpha_CaHVA_i, mBeta_CaHVA_i = cf.Ca_HVA_m_alpha_beta(vi)
+    hAlpha_CaHVA_i, hBeta_CaHVA_i = cf.Ca_HVA_h_alpha_beta(vi)
+    mAlpha_Im_i, mBeta_Im_i = cf.Im_m_alpha_beta_with_params(vi, shift, slope, k)
 
-all_gates = mInf * mInf * hInf
-all_gates_Ca_HVA = np.copy(all_gates)
+    mAlpha_CaHVA.append(mAlpha_CaHVA_i)
+    mBeta_CaHVA.append(mBeta_CaHVA_i)
+    hAlpha_CaHVA.append(hAlpha_CaHVA_i)
+    hBeta_CaHVA.append(hBeta_CaHVA_i)
+    mAlpha_Im.append(mAlpha_Im_i)
+    mBeta_Im.append(mBeta_Im_i)
 
-mInf_Ca_HVA = np.copy(mInf)
-mTau_Ca_HVA = np.copy(mTau)
-hInf_Ca_HVA = np.copy(hInf)
-hTau_Ca_HVA = np.copy(hTau)
+mAlpha_CaHVA = np.array(mAlpha_CaHVA)
+mBeta_CaHVA = np.array(mBeta_CaHVA)
+hAlpha_CaHVA = np.array(hAlpha_CaHVA)
+hBeta_CaHVA = np.array(hBeta_CaHVA)
+mAlpha_Im = np.array(mAlpha_Im)
+mBeta_Im = np.array(mBeta_Im)
 
-# =========== Im ===========
-shift = -39
-slope = 2.3
-k =2.0e-3
+mInf_Ca_HVA = cf.gate_inf(mAlpha_CaHVA, mBeta_CaHVA)
+hInf_Ca_HVA = cf.gate_inf(hAlpha_CaHVA, hBeta_CaHVA)
+mInf_Im = cf.gate_inf(mAlpha_Im, mBeta_Im)
 
-qt = math.pow(2.3, ((34 - 21) / 10))
-v = np.arange(-100, 150, 1).astype(np.float32)
-mAlpha = k * np.exp(slope * 0.04 * (v - shift))
-mBeta = k * np.exp(-slope * 0.04 * (v - shift))
-
-mInf = mAlpha / (mAlpha + mBeta)
-mTau = (1 / (mAlpha + mBeta)) / qt
-
-all_gates = mInf
-all_gates_Im = np.copy(all_gates)
-mInf_Im = np.copy(mInf)
-mTau_Im = np.copy(mTau)
+mTau_Ca_HVA = cf.gate_tau(mAlpha_CaHVA, mBeta_CaHVA)
+hTau_Ca_HVA = cf.gate_tau(hAlpha_CaHVA, hBeta_CaHVA)
+mTau_Im = cf.Im_m_tau(mAlpha_Im, mBeta_Im)
 
 # ========= time constants ==========
 evenly_spaced_interval = np.linspace(0, 1, 7)
@@ -64,7 +59,8 @@ ax.set_xlim([-100, 50])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.show()
-# fig.savefig("./Fig4/fig4_hTau_Ca_HVA.svg", format='svg')
+if general_params["save_figs"]:
+    fig.savefig("./Fig4/fig4_hTau_Ca_HVA.svg", format='svg')
 
 fig, ax = plt.subplots(figsize=(1.5, 2))
 ax.plot(v, mTau_Ca_HVA, color=colors[6])
@@ -76,7 +72,8 @@ ax.set_ylim([0, 100])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.show()
-# fig.savefig("./Fig4/fig4_mTau_Ca_HVA_Im.svg", format='svg')
+if general_params["save_figs"]:
+    fig.savefig("./Fig4/fig4_mTau_Ca_HVA_Im.svg", format='svg')
 
 
 # ========= activation curves ==========
@@ -90,4 +87,5 @@ ax.set_xlim([-60, 0])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.show()
-# fig.savefig("./Fig4/fig4_m_Ca_HVA_Im.svg", format='svg')
+if general_params["save_figs"]:
+    fig.savefig("./Fig4/fig4_m_Ca_HVA_Im.svg", format='svg')
