bf86b0709ccd0e231d7de7fe2c594844785429c2
amirdud <amirdud@users.noreply.github.com>
2021-12-15 01:37:36 +0200

    improved modularity

--- Fig9_1_trace.py
+++ Fig9_1_trace.py
@@ -1,236 +1,89 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 import time
-# matplotlib.use('Qt5Agg')
 import pickle
+import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-import mod_dynamics_funs as mdf
+import phys_funs as pf
+import channel_funs as cf
+import dynamics_funs as df
+from cfg import *
+from constants import *
 
-# ========== parameters ============
+# =========== parameters ===========
 file = open("nexus_point_neuron_parameters.pkl",'rb')
-nexus_parameters = pickle.load(file)
+nexus_params = pickle.load(file)
 file.close()
 
-# Set random seed (for reproducibility)
-np.random.seed(1)
-
-# Start and end time (in milliseconds)
-shift = -39
-slope = 2.3
-k =2.0e-3
-to_mS = 1000
-tmin = 0.0
-tmax = 200.0
-dt = 0.025
+general_params = cfg_fig_9["h"]
+tmin = general_params["tmin"]
+tmax = general_params["tmax"]
+dt = general_params["dt"]
+pert_type = general_params["pert_type"]
 T = np.arange(tmin, tmax, dt)
 
-# parameters from:
-# http://www.math.pitt.edu/~bdoiron/assets/ermentrout-and-terman-ch-1.pdf
-gK = 36
-gNa = 120
-gL = 0.3
-g_con = 0.4
+phys_params = cfg_fig_9["phys"]
+shift = phys_params["shift"]
+slope = phys_params["slope"]
+k = phys_params["k"]
+gK = phys_params["gK"]
+gNa = phys_params["gNa"]
+gL = phys_params["gL"]
+g_con = phys_params["g_con"]
+A_soma = phys_params["A_soma"]
+A_nexus = phys_params["A_nexus"]
+Cm_soma = phys_params["Cm_soma"]
+Cm_nexus = phys_params["Cm_nexus"]
+E_K = phys_params["E_K"]
+E_Na = phys_params["E_Na"]
+E_l = phys_params["E_l"]
+E_Ca = phys_params["E_Ca"]
 
-A_soma = 1e-5  # cm2 (18 um cell diameter)
-A_nexus = 1e-5  # cm2 (18 um cell diameter)
+sim_params = cfg_fig_9["sim"]
+amp_soma = sim_params["amp_soma"]
+amp_nexus = sim_params["amp_nexus"]
+Im_m_init = sim_params["Im_m_init"]
+mul_gpas = sim_params["mul_gpas"]
+mul_K = sim_params["mul_K"]
+amp_nexus_syn_gen = sim_params["amp_nexus_syn_gen"]
+ts_nexus_gen = sim_params["ts_nexus_gen"]
+tau1_nexus_gen = sim_params["tau1_nexus_gen"]
+tau2_nexus_gen = sim_params["tau2_nexus_gen"]
+tau1_nexus_pert = sim_params["tau1_nexus_pert"]
+tau2_nexus_pert = sim_params["tau2_nexus_pert"]
+V_soma_init = sim_params["V_soma_init"]
+V_nexus_init = sim_params["V_nexus_init"]
+amp_nexus_syn_pert = sim_params["pert_dependent_params"][pert_type]["amp_nexus_syn_pert"]
+mul_Ca = sim_params["pert_dependent_params"][pert_type]["mul_Ca"]
 
-Cm_nexus = 1.0
-Cm_soma = 1.0
-
-E_K = -77
-E_Na = 50
-E_l = -54.4
-E_Ca = 120
-
-amp_nexus = 0
-amp_soma = 0
-
-Im_m_init = 0
-mul_gpas = 1
-mul_K = 7.5
-
-# generate spike
-amp_nexus_syn_gen = 0.0004
-ts_nexus_gen = 50
-tau1_nexus_gen = 0.5
-tau2_nexus_gen = 8
-
-# perturb spike
-ts_nexus_pert = 58
-tau1_nexus_pert = 0.5
-tau2_nexus_pert = 5
-
-V_soma_init = -65.0
-V_nexus_init = -65.0
-
-pert_type = 'ctrl'
-# pert_type = 'ipsp'
-# pert_type = 'epsp'
-# pert_type = 'ACh'
-
-if pert_type == 'ctrl':
-    amp_nexus_syn_pert = 0
-    mul_Ca = 7
-
-elif pert_type == 'ipsp':
-    amp_nexus_syn_pert = -0.0004
-    mul_Ca = 7
-
-elif pert_type == 'epsp':
-    amp_nexus_syn_pert = 0.0012
-    mul_Ca = 7
-
-elif pert_type == 'ACh':
-    amp_nexus_syn_pert = 0
-    mul_Ca = 7.5
-
-
-# ========== Functions ===============
-# K
-def alpha_n(Vm):
-    return (0.01 * (Vm + 55.0)) / (1 - np.exp(-(Vm + 55.0)/10.0))
-
-
-def beta_n(Vm):
-    return (0.125 * np.exp(-(Vm + 65.0)/80.0))
-
-
-# Na
-def alpha_m(Vm):
-    return (0.1 * (Vm + 40.0)) / (1 - np.exp(-(Vm + 40.0)/10.0))
-
-
-def beta_m(Vm):
-    return (4 * np.exp(-(Vm + 65.0)/18.0))
-
-
-def alpha_h(Vm):
-    return 0.07 * np.exp(-(Vm + 65.0) / 20.0)
-
-
-def beta_h(Vm):
-    return (1 / (1 + np.exp(-(Vm + 35.0) / 10.0)) )
-
-
-def n_inf(Vm=-65.0):
-    return alpha_n(Vm) / (alpha_n(Vm) + beta_n(Vm))
-
-
-def m_inf(Vm=-65.0):
-    return alpha_m(Vm) / (alpha_m(Vm) + beta_m(Vm))
-
-
-def h_inf(Vm=-65.0):
-    return alpha_h(Vm) / (alpha_h(Vm) + beta_h(Vm))
-
-
-def I_ext_constant(amp):
-    return amp
-
-
-def I_ext_pulse(t_curr, amp, ts=50, dur=10):
-    if ts < t_curr < ts+dur:
-        return amp
-    else:
-        return 0.0
-
-
-def I_ext_syn(t_curr, amp_syn, ts=0, tau1=0.5, tau2=5):
-    if t_curr < ts:
-        return 0.0
-    else:
-        # (a)
-        # I_syn = amp_syn*np.exp(-(t_curr - ts) / tau)
-        # (b)
-        # I_syn = amp_syn * ((t_curr - ts) / tau) *np.exp(-(t_curr - ts) / tau)
-        # (c)
-        I_syn = amp_syn * ((tau1*tau2)/(tau1-tau2)) *( np.exp(-(t_curr - ts) / tau1) - np.exp(-(t_curr - ts) / tau2))
-
-        return I_syn
-
-
-def compute_derivatives(y, t_curr, amp_soma, amp_nexus,
-                        amp_nexus_syn_gen, ts_nexus_gen, tau1_nexus_gen, tau2_nexus_gen,
-                        amp_nexus_syn_pert, ts_nexus_pert, tau1_nexus_pert, tau2_nexus_pert):
-
-    dy = np.zeros((6,))
-
-    Vm_soma = y[0]
-    n = y[1]
-    m = y[2]
-    h = y[3]
-    Vm_nexus = y[4]
-    Im_m = y[5]
-
-    # nexus
-    Ca_HVA_h_inf = mdf.Ca_HVA_h_inf(E_l)
-    Ca_HVA_m_inf = mdf.Ca_HVA_m_inf(Vm_nexus)  # inst
-    gCa_HVA = mdf.calc_conductance('HVA_first_order', gCa_HVAbar, [Ca_HVA_m_inf, Ca_HVA_h_inf])
-    gIm = mdf.calc_conductance('Im', gImbar, [Im_m])
-
-    I_Ca_HVA = mdf.calc_current(Vm_nexus, gCa_HVA, E_Ca)
-    I_Im = mdf.calc_current(Vm_nexus, gIm, E_K)
-    I_l = mdf.calc_current(Vm_nexus, gl_pas, E_l)
-    I_I_ext_nexus = I_ext_constant(amp_nexus) / A_nexus
-    I_I_ext_nexus_syn_gen = I_ext_syn(t_curr, amp_syn=amp_nexus_syn_gen, ts=ts_nexus_gen,
-                                      tau1=tau1_nexus_gen, tau2=tau2_nexus_gen) / A_nexus
-    I_I_ext_nexus_syn_pert = I_ext_syn(t_curr, amp_syn=amp_nexus_syn_pert, ts=ts_nexus_pert,
-                                       tau1=tau1_nexus_pert, tau2=tau2_nexus_pert) / A_nexus
-
-    # soma
-    GK = gK * np.power(n, 4.0)
-    GNa = gNa * np.power(m, 3.0) * h
-    GL = gL
-
-    I_I_ext_soma = I_ext_constant(amp_soma) / A_soma
-
-    # dVm_soma/dt
-    dy[0] = (I_I_ext_soma - (GK * (Vm_soma - E_K)) - (GNa * (Vm_soma - E_Na)) - (GL * (Vm_soma - E_l)) -
-             g_con*(Vm_soma - Vm_nexus)) / Cm_soma
-
-    # dn/dt
-    dy[1] = (alpha_n(Vm_soma) * (1.0 - n)) - (beta_n(Vm_soma) * n)
-
-    # dm/dt
-    dy[2] = (alpha_m(Vm_soma) * (1.0 - m)) - (beta_m(Vm_soma) * m)
-
-    # dh/dt
-    dy[3] = (alpha_h(Vm_soma) * (1.0 - h)) - (beta_h(Vm_soma) * h)
-
-    # dVm_nexus/dt
-    dy[4] = (I_I_ext_nexus + I_I_ext_nexus_syn_gen + I_I_ext_nexus_syn_pert - I_Ca_HVA - I_Im - I_l -
-             g_con*(Vm_nexus - Vm_soma)) / Cm_nexus
-
-    # Im dm/dt
-    alpha, beta = mdf.Im_m_alpha_beta_modified(Vm_nexus, shift, slope, k)
-    dy[5] = (alpha * (1.0 - Im_m)) - (beta * Im_m)
-
-    return dy
-
+ts_nexus_pert = cfg_fig_9_1["sim"]["ts_nexus_pert"]
 
 # ============= run simulation ==============
 dic_log = {}
 data = {}
 
 # State vector parameters: v, all gates
-gCa_HVAbar = nexus_parameters['gCa_HVAbar_Ca_HVA'] * mul_Ca * to_mS
-gImbar = nexus_parameters['gImbar_Im'] * mul_K * to_mS
-gl_pas = nexus_parameters['g_pas'] * mul_gpas * to_mS
+gCa_HVAbar = nexus_params['gCa_HVAbar_Ca_HVA'] * mul_Ca * TO_MS
+gImbar = nexus_params['gImbar_Im'] * mul_K * TO_MS
+gl_pas = nexus_params['g_pas'] * mul_gpas * TO_MS
 
-Y = np.array([V_soma_init, n_inf(V_soma_init), m_inf(V_soma_init), h_inf(V_soma_init),
+Y = np.array([V_soma_init, cf.HH_K_n_inf(V_soma_init),
+              cf.HH_Na_m_inf(V_soma_init),
+              cf.HH_Na_h_inf(V_soma_init),
               V_nexus_init, Im_m_init])
 
 # Solve ODE system
-Vy = odeint(compute_derivatives, Y, T, args=(amp_soma, amp_nexus,
-                                             amp_nexus_syn_gen, ts_nexus_gen,tau1_nexus_gen, tau2_nexus_gen,
-                                             amp_nexus_syn_pert, ts_nexus_pert, tau1_nexus_pert, tau2_nexus_pert))
+Vy = odeint(df.two_compartments_model_with_perturbation, Y, T,
+            args=(amp_soma, amp_nexus, amp_nexus_syn_gen, ts_nexus_gen,tau1_nexus_gen, tau2_nexus_gen,
+                  amp_nexus_syn_pert, ts_nexus_pert, tau1_nexus_pert, tau2_nexus_pert,
+                  gCa_HVAbar, gImbar, gl_pas, gK, gNa, gL, g_con,
+                  E_Ca, E_K, E_l, E_Na, A_soma, A_nexus, Cm_soma, Cm_nexus, shift, slope, k))
 
 dy_list = []
 for i in range(len(T)):
-    dy = compute_derivatives(Vy[i], T[i], amp_soma, amp_nexus,
-                             amp_nexus_syn_gen, ts_nexus_gen,tau1_nexus_gen, tau2_nexus_gen,
-                             amp_nexus_syn_pert, ts_nexus_pert, tau1_nexus_pert, tau2_nexus_pert)
+    dy = df.two_compartments_model_with_perturbation(
+        Vy[i], T[i], amp_soma, amp_nexus, amp_nexus_syn_gen, ts_nexus_gen,tau1_nexus_gen, tau2_nexus_gen,
+        amp_nexus_syn_pert, ts_nexus_pert, tau1_nexus_pert, tau2_nexus_pert,
+        gCa_HVAbar, gImbar, gl_pas, gK, gNa, gL, g_con,
+        E_Ca, E_K, E_l, E_Na, A_soma, A_nexus, Cm_soma, Cm_nexus, shift, slope, k)
     dy_list.append(dy)
 Vdy = np.array(dy_list)
 v_soma_dot_t = Vdy[:, 0]
@@ -248,24 +101,24 @@ v_nexus_t = Vy[:, 4]
 Im_m_t = Vy[:, 5]
 
 # nexus
-Ca_HVA_h_inf_t = np.ones_like(v_nexus_t) * mdf.Ca_HVA_h_inf(E_l)
-Ca_HVA_m_inf_t = np.array([mdf.Ca_HVA_m_inf(v_ti) for v_ti in v_nexus_t])  # inst
-gCa_HVA_t = mdf.calc_conductance('HVA_first_order', gCa_HVAbar, [Ca_HVA_m_inf_t, Ca_HVA_h_inf_t])
-gIm_t = mdf.calc_conductance('Im', gImbar, [Im_m_t])
+Ca_HVA_h_inf_t = np.ones_like(v_nexus_t) * cf.Ca_HVA_h_inf(E_l)
+Ca_HVA_m_inf_t = np.array([cf.Ca_HVA_m_inf(v_ti) for v_ti in v_nexus_t])  # inst
+gCa_HVA_t = pf.calc_conductance('HVA_first_order', gCa_HVAbar, [Ca_HVA_m_inf_t, Ca_HVA_h_inf_t])
+gIm_t = pf.calc_conductance('Im', gImbar, [Im_m_t])
 gl_pas_t = gl_pas * np.ones(np.size(v_nexus_t))
 
-I_Ca_HVA_t = mdf.calc_current(v_nexus_t, gCa_HVA_t, E_Ca)
-I_Im_t = mdf.calc_current(v_nexus_t, gIm_t, E_K)
-I_l_nexus_t = mdf.calc_current(v_nexus_t, gl_pas_t, E_l)
+I_Ca_HVA_t = pf.calc_current(v_nexus_t, gCa_HVA_t, E_Ca)
+I_Im_t = pf.calc_current(v_nexus_t, gIm_t, E_K)
+I_l_nexus_t = pf.calc_current(v_nexus_t, gl_pas_t, E_l)
 
 # soma
 GK_t = gK * np.power(n_t, 4.0)
 GNa_t = gNa * np.power(m_t, 3.0) * h_t
 GL_t = gL * np.ones(np.size(v_soma_t))
 
-IK_t = mdf.calc_current(v_soma_t, GK_t, E_K)
-INa_t = mdf.calc_current(v_soma_t, GNa_t, E_Na)
-I_l_soma_t = mdf.calc_current(v_soma_t, GL_t, E_l)
+IK_t = pf.calc_current(v_soma_t, GK_t, E_K)
+INa_t = pf.calc_current(v_soma_t, GNa_t, E_Na)
+I_l_soma_t = pf.calc_current(v_soma_t, GL_t, E_l)
 
 data['v_soma_t'] = v_soma_t
 data['n_t'] = n_t
@@ -306,7 +159,12 @@ if amp_nexus_syn_pert != 0:
 ax0.set_xlim([45, 100])
 ax1.set_xlim([45, 100])
 plt.show()
-# fig.savefig("./Fig9/Fig9_ctrl_trace.svg", format='svg')
-# fig.savefig("./Fig9/Fig9_inh_trace.svg", format='svg')
-# fig.savefig("./Fig9/Fig9_exc_trace.svg", format='svg')
-# fig.savefig("./Fig9/Fig9_Ach_trace.svg", format='svg')
+if general_params["save_figs"]:
+    if pert_type == 'ctrl':
+        fig.savefig("./Fig9/Fig9_ctrl_trace.svg", format='svg')
+    if pert_type == 'ipsp':
+        fig.savefig("./Fig9/Fig9_inh_trace.svg", format='svg')
+    if pert_type == 'epsp':
+        fig.savefig("./Fig9/Fig9_exc_trace.svg", format='svg')
+    if pert_type == 'ACh':
+        fig.savefig("./Fig9/Fig9_ACh_trace.svg", format='svg')
